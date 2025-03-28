6793c8cc45b9881fe8ede385747cc6c30a28e735
noest <noest@DESKTOP-M59644L>
2025-03-28 18:07:50 +0100

    Suuuu

---
+++ JavaGit/src/PruebaGit/GitClass.java
@@ -0,0 +1,10 @@
+package PruebaGit;
+
+public class GitClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Que mierda me estas contando");
+	}
+
+}
---
+++ JavaGit/src/PruebaGit/Vehiculo.java
@@ -0,0 +1,13 @@
+package PruebaGit;
+
+public class Vehiculo {
+	
+		double velocidad;
+		int año;
+		String combustible;
+		int defensa;
+		int ataque;
+		int peso;
+	
+	
+}
