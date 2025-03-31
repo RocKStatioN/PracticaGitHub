ba92ba38ad6333fdfd5d74b28862f2c6f5bf9b59
noest <noest@DESKTOP-M59644L>
2025-03-31 23:03:09 +0200

    Modificado para la version 1.1

--- JavaGit/src/PruebaGit/GitClass.java
+++ JavaGit/src/PruebaGit/GitClass.java
@@ -5,5 +5,7 @@ public class GitClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Que mierda me estas contando");
+System.out.println("cREANDO RAMA V1.1");	
+	
 	}
 }
