9d226d167f31b17a61f81607eed2d6c723e5cb36
noest <noest@DESKTOP-M59644L>
2025-03-31 21:55:32 +0200

    Version 1.1

--- JavaGit/src/PruebaGit/GitClass.java
+++ JavaGit/src/PruebaGit/GitClass.java
@@ -5,7 +5,5 @@ public class GitClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Que mierda me estas contando");
-System.out.println("Actualizacion del mensaje anterior. Quiero cortarme los cojones");
-System.out.println("Actulizacion rama master");
 	}
 }
