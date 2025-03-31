25b8a36ca05a3e23a0419927dd31fe66a86c911e
noest <noest@DESKTOP-M59644L>
2025-03-31 19:57:28 +0200

    Cambio en rama master.

--- JavaGit/src/PruebaGit/GitClass.java
+++ JavaGit/src/PruebaGit/GitClass.java
@@ -5,6 +5,7 @@ public class GitClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Que mierda me estas contando");
+System.out.println("Actualizacion del mensaje anterior. Quiero cortarme los cojones");
+System.out.println("Actulizacion rama master");
 	}
-
 }
